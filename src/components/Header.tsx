7ec7d2ac573efc1dcc6baf22245885abf0146d8c
d.nikulshin.dev <d.nikulshin.dev@gmail.com>
2025-03-19 00:53:46 +0000

    edit styles header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export const Header = ({
                 <div className="flex justify-between gap-3 items-center w-full">
                     <button onClick={() => setVisibleCreateForm(!visibleCreateForm)} className=" bg-green-600 px-4 py-2 rounded-md  cursor-pointer shadow-sm shadow-green-600/50">Создать заявку</button>
                     <button
-                        className="bg-red-500 px-4 py-2 rounded-sm cursor-pointer disabled:bg-gray-400 shadow-sm shadow-red-500/50"
+                        className="bg-red-500 px-3 py-1.5 rounded-sm cursor-pointer disabled:bg-gray-400 shadow-sm shadow-red-500/50"
                         disabled={isLogout}
                         onClick={logoutHandler}>
                         Выйти
