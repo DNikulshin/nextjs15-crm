5a79be0e6625015362345bda22312025c7f2092e
d.nikulshin.dev <d.nikulshin.dev@gmail.com>
2025-03-22 13:48:37 +0000

    ckeckApi add data type unknown

--- src/shared/utils/checkApiKeyHandler.ts
+++ src/shared/utils/checkApiKeyHandler.ts
@@ -2,7 +2,7 @@ export async function checkApiKey(req: Request): Promise<boolean> {
     return req.headers.get('x-api-key') === process.env.NEXT_PUBLIC_API_KEY;
 }
 
-export function createResponse(data: any, status = 200): Response {
+export function createResponse(data: unknown, status = 200): Response {
     return new Response(JSON.stringify(data), {
         status,
         headers: { "Content-Type": "application/json" }
